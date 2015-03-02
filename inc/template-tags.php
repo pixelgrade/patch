696fd6913bd7fbeb168c428d47c9152aefe7ab3b
Vlad Olaru <vlad.olaru@pixelgrade.com>
2015-03-02 17:01:27 +0200

    posts special classes

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -264,3 +264,247 @@ function fifteen_category_transient_flusher() {
 }
 add_action( 'edit_category', 'fifteen_category_transient_flusher' );
 add_action( 'save_post',     'fifteen_category_transient_flusher' );
+
+/**
+ * Display the classes for the post thumbail div.
+ *
+ * @param string|array $class One or more classes to add to the class list.
+ * @param int|WP_Post $post_id Optional. Post ID or post object.
+ */
+function fifteen_post_thumbnail_class( $class = '', $post_id = null ) {
+	// Separates classes with a single space, collates classes for post thumbnail DIV
+	echo 'class="' . join( ' ', fifteen_get_post_thumbnail_class( $class, $post_id ) ) . '"';
+}
+
+if ( ! function_exists( 'fifteen_get_post_thumbnail_class' ) ) :
+	/**
+	 * Retrieve the classes for the post_thumbnail,
+	 * depending on the aspect ratio of the featured image
+	 *
+	 * @param string|array $class One or more classes to add to the class list.
+	 * @return array Array of classes.
+	 */
+	function fifteen_get_post_thumbnail_class( $class = '', $post_id = null ) {
+
+		$post = get_post( $post_id );
+
+		$classes = array();
+
+		if ( empty( $post ) )
+			return $classes;
+
+		$classes[] = 'entry-image';
+
+		// .entry-image--[tall|portrait|square|landscape|wide] class depending on the aspect ratio
+		// 16:9 = 1.78
+		// 3:2 = 1.500
+		// 4:3 = 1.34
+		// 1:1 = 1.000
+		// 3:4 = 0.750
+		// 2:3 = 0.67
+		// 9:16 = 0.5625
+
+		//$image_data[1] is width
+		//$image_data[2] is height
+		$image_data = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), "full" );
+
+		if ( ! empty( $image_data[1] ) && ! empty( $image_data[2] ) ) {
+			$image_aspect_ratio = $image_data[1] / $image_data[2];
+
+			//now let's begin to see what kind of featured image we have
+			//first TALL ones; lower than 9:16
+			if ( $image_aspect_ratio < 0.5625 ) {
+				$classes[] = 'entry-image--tall';
+			} elseif ( $image_aspect_ratio < 0.75 ) {
+				//now PORTRAIT ones; lower than 3:4
+				$classes[] = 'entry-image--portrait';
+			} elseif ( $image_aspect_ratio > 1.78 ) {
+				//now WIDE ones; higher than 16:9
+				$classes[] = 'entry-image--wide';
+			} elseif ( $image_aspect_ratio < 1.34 ) {
+				//now LANDSCAPE ones; higher than 4:3
+				$classes[] = 'entry-image--landscape';
+			} else {
+				//it's definitely a SQUARE-ish one; between 3:4 and 4:3
+				$classes[] = 'entry-image--square';
+			}
+		}
+
+		if ( ! empty( $class ) ) {
+			if ( ! is_array( $class ) )
+				$class = preg_split( '#\s+#', $class );
+			$classes = array_merge( $classes, $class );
+		}
+
+		$classes = array_map( 'esc_attr', $classes );
+
+		/**
+		 * Filter the list of CSS classes for the current post thumbnail.
+		 *
+		 * @param array  $classes An array of post classes.
+		 * @param string $class   A comma-separated list of additional classes added to the post.
+		 * @param int    $post_id The post ID.
+		 */
+		$classes = apply_filters( 'fifteen_post_thumbnail_class', $classes, $class, $post->ID );
+
+		return array_unique( $classes );
+
+	}
+endif;
+
+/**
+ * Display the classes for the post title div.
+ *
+ * @param string|array $class One or more classes to add to the class list.
+ * @param int|WP_Post $post_id Optional. Post ID or post object.
+ */
+function fifteen_post_title_class( $class = '', $post_id = null ) {
+	// Separates classes with a single space, collates classes for post title
+	echo 'class="' . join( ' ', fifteen_get_post_title_class( $class, $post_id ) ) . '"';
+}
+
+if ( ! function_exists( 'fifteen_get_post_title_class' ) ) :
+	/**
+	 * Retrieve the classes for the post title,
+	 * depending on the length of the title
+	 *
+	 * @param string|array $class One or more classes to add to the class list.
+	 * @return array Array of classes.
+	 */
+	function fifteen_get_post_title_class( $class = '', $post_id = null ) {
+
+		$post = get_post( $post_id );
+
+		$classes = array();
+
+		if ( empty( $post ) )
+			return $classes;
+
+		$classes[] = 'entry-title';
+
+		// .entry-title--[short|medium|long] depending on the title length
+		// 0-29 chars = short
+		// 30-59 = medium
+		// 60+ = long
+		// @ todo Put in the needed logic for when mb functions are not present
+		$title_length = mb_strlen( get_the_title( $post ) );
+
+		if ( $title_length < 30 ) {
+			$classes[] = 'entry-title--short';
+		} elseif ( $title_length < 60 ) {
+			$classes[] = 'entry-title--medium';
+		} else {
+			$classes[] = 'entry-title--long';
+		}
+
+		if ( !empty($class) ) {
+			if ( !is_array( $class ) )
+				$class = preg_split('#\s+#', $class);
+			$classes = array_merge($classes, $class);
+		}
+
+		$classes = array_map('esc_attr', $classes);
+
+		/**
+		 * Filter the list of CSS classes for the current post title.
+		 *
+		 * @param array  $classes An array of post classes.
+		 * @param string $class   A comma-separated list of additional classes added to the post.
+		 * @param int    $post_id The post ID.
+		 */
+		$classes = apply_filters( 'fifteen_post_title_class', $classes, $class, $post->ID );
+
+		return array_unique( $classes );
+
+	}
+endif;
+
+/**
+ * Display the classes for the post excerpt div.
+ *
+ * @param string|array $class One or more classes to add to the class list.
+ * @param int|WP_Post $post_id Optional. Post ID or post object.
+ */
+function fifteen_post_excerpt_class( $class = '', $post_id = null ) {
+	// Separates classes with a single space, collates classes for the post excerpt div
+	echo 'class="' . join( ' ', fifteen_get_post_excerpt_class( $class, $post_id ) ) . '"';
+}
+
+if ( ! function_exists( 'fifteen_get_post_excerpt_class' ) ) :
+	/**
+	 * Retrieve the classes for the post excerpt,
+	 * depending on the length of the excerpt
+	 *
+	 * @param string|array $class One or more classes to add to the class list.
+	 * @return array Array of classes.
+	 */
+	function fifteen_get_post_excerpt_class( $class = '', $post_id = null ) {
+
+		$post = get_post( $post_id );
+
+		$classes = array();
+
+		if ( empty( $post ) )
+			return $classes;
+
+		$classes[] = 'entry-excerpt';
+
+		// .entry-title--[short|medium|long] depending on the title length
+		// 0-99 chars = short
+		// 100-199 = medium
+		// 200+ = long
+		// @ todo Put in the needed logic for when mb functions are not present
+		$excerpt_length = mb_strlen( fifteen_get_post_excerpt( $post ) );
+
+		if ( $excerpt_length < 99 ) {
+			$classes[] = 'entry-excerpt--short';
+		} elseif ( $excerpt_length < 199 ) {
+			$classes[] = 'entry-excerpt--medium';
+		} else {
+			$classes[] = 'entry-excerpt--long';
+		}
+
+		if ( !empty($class) ) {
+			if ( !is_array( $class ) )
+				$class = preg_split('#\s+#', $class);
+			$classes = array_merge($classes, $class);
+		}
+
+		$classes = array_map('esc_attr', $classes);
+
+		/**
+		 * Filter the list of CSS classes for the current post excerpt.
+		 *
+		 * @param array  $classes An array of post classes.
+		 * @param string $class   A comma-separated list of additional classes added to the post.
+		 * @param int    $post_id The post ID.
+		 */
+		$classes = apply_filters( 'fifteen_post_excerpt_class', $classes, $class, $post->ID );
+
+		return array_unique( $classes );
+
+	}
+endif;
+
+if ( ! function_exists( 'fifteen_post_excerpt' ) ) :
+
+	function fifteen_post_excerpt( $post_id = null ) {
+		$post = get_post( $post_id );
+
+		if ( empty( $post ) )
+			return '';
+
+		// Check the content for the more text
+		$has_more = strpos( $post->post_content, '<!--more' );
+
+		if ( $has_more ) {
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading %s', 'fifteen_txtd' ),
+				the_title( '<span class="screen-reader-text">', '</span>', false )
+			) );
+		} else {
+			the_excerpt();
+		}
+	}
+endif;
