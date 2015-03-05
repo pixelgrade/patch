44d59b54ebff18530d68af4653fca2609fe16895
cristianfrumusanu <cristian.frumusanu@pixelgrade.com>
2015-03-05 15:40:01 +0200

    single featured image layouts | #6

--- content-single.php
+++ content-single.php
@@ -5,20 +5,19 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header <?php patch_post_thumbnail_class( 'entry-header' ); ?>>
+	<header class="entry-header">
 		<div class="entry-meta">
 			<?php patch_cats_list(); ?>
 			<?php patch_posted_on(); ?>
 		</div><!-- .entry-meta -->
 
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<?php if ( has_post_thumbnail() ) { ?>
-			<div class="entry-featured  entry-thumbnail">
-				<?php the_post_thumbnail( 'patch-single-image' ); ?>
-			</div>
-		<?php } ?>
 	</header><!-- .entry-header -->
+	<?php if ( has_post_thumbnail() ) { ?>
+		<div class="entry-featured  entry-thumbnail">
+			<?php the_post_thumbnail( 'patch-single-image' ); ?>
+		</div>
+	<?php } ?>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
