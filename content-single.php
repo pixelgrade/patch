8872f103fc9d68e9bb231f1da82a92fda3361c8f
Vlad Olaru <vlad.olaru@pixelgrade.com>
2015-03-05 17:26:06 +0200

    header for single

--- content-single.php
+++ content-single.php
@@ -7,29 +7,40 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<div class="entry-meta">
+
 			<?php patch_cats_list(); ?>
+
 			<?php patch_posted_on(); ?>
+
 		</div><!-- .entry-meta -->
 
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
 	</header><!-- .entry-header -->
 	<?php if ( has_post_thumbnail() ) { ?>
+
 		<div class="entry-featured  entry-thumbnail">
 			<?php the_post_thumbnail( 'patch-single-image' ); ?>
 		</div>
+
 	<?php } ?>
 
 	<div class="entry-content">
+
 		<?php the_content(); ?>
+
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', 'patch_txtd' ),
 				'after'  => '</div>',
 			) );
 		?>
+
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
+
 		<?php patch_single_entry_footer(); ?>
+
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
