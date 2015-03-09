7d444ced2bd849eed4381e01bf5e8ee94e61a7f2
Razvan Onofrei <razwan.me@gmail.com>
2015-03-09 16:17:35 +0200

    author box, tags & co - #12

--- author-bio.php
+++ author-bio.php
@@ -7,14 +7,14 @@
  */
 ?>
 
-<aside class="author__info" itemscope itemtype="http://schema.org/Person">
-	<div class="author__avatar">
+<aside class="author-info  media" itemscope itemtype="http://schema.org/Person">
+	<div class="author-info__avatar  media__img">
 		<?php echo get_avatar( get_the_author_meta( 'user_email' ), 120 ); ?>
 	</div>
-	<div class="author__description">
-		<h3 class="author__title"><?php echo get_the_author(); ?></h3>
-		<a class="author__link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author" title="<?php printf( __( 'View all posts by %s', 'patch_txtd' ), get_the_author() ); ?>"><?php _e( 'All posts', 'patch_txtd' ); ?></a>
-		<p class="author__bio" itemprop="description"><?php the_author_meta('description'); ?></p>
+	<div class="author-info__description  media__body">
+		<h3 class="author-info__title"><?php echo get_the_author(); ?></h3>
+		<a class="author-info__link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author" title="<?php printf( __( 'View all posts by %s', 'patch_txtd' ), get_the_author() ); ?>"><?php _e( 'All posts', 'patch_txtd' ); ?></a>
+		<p class="author-info__bio" itemprop="description"><?php the_author_meta('description'); ?></p>
 
 		<?php patch_author_bio_links(); ?>
 
