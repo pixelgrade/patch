eba81a74b57a237aa553d6a4bedb7c99e94620b4
cristianfrumusanu <cristian.frumusanu@pixelgrade.com>
2015-03-12 13:07:23 +0200

    sidebar + widgets | #8

--- functions.php
+++ functions.php
@@ -133,8 +133,8 @@ function patch_widgets_init() {
 		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h4 class="widget-title">',
+		'after_title'   => '</h4>',
 	) );
 }
 add_action( 'widgets_init', 'patch_widgets_init' );
