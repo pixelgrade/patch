0784a09ed13e6f6b798d0e07eea70f986fddc5d6
cristianfrumusanu <cristian.frumusanu@pixelgrade.com>
2015-03-12 16:51:12 +0200

    archive navigation

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -8,64 +8,6 @@
  * @since Patch 1.0
  */
 
-if ( ! function_exists( 'the_posts_navigation' ) ) :
-/**
- * Display navigation to next/previous set of posts when applicable.
- *
- * @todo Remove this function when WordPress 4.3 is released.
- */
-function the_posts_navigation() {
-	// Don't print empty markup if there's only one page.
-	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
-		return;
-	}
-	?>
-	<nav class="navigation posts-navigation" role="navigation">
-		<h2 class="screen-reader-text"><?php _e( 'Posts navigation', 'patch_txtd' ); ?></h2>
-		<div class="nav-links">
-
-			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'patch_txtd' ) ); ?></div>
-			<?php endif; ?>
-
-			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'patch_txtd' ) ); ?></div>
-			<?php endif; ?>
-
-		</div><!-- .nav-links -->
-	</nav><!-- .navigation -->
-	<?php
-}
-endif;
-
-if ( ! function_exists( 'the_post_navigation' ) ) :
-/**
- * Display navigation to next/previous post when applicable.
- *
- * @todo Remove this function when WordPress 4.3 is released.
- */
-function the_post_navigation() {
-	// Don't print empty markup if there's nowhere to navigate.
-	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
-	$next     = get_adjacent_post( false, '', false );
-
-	if ( ! $next && ! $previous ) {
-		return;
-	}
-	?>
-	<nav class="navigation post-navigation" role="navigation">
-		<h2 class="screen-reader-text"><?php _e( 'Post navigation', 'patch_txtd' ); ?></h2>
-		<div class="nav-links">
-			<?php
-				previous_post_link( '<div class="nav-previous">%link</div>', '%title' );
-				next_post_link( '<div class="nav-next">%link</div>', '%title' );
-			?>
-		</div><!-- .nav-links -->
-	</nav><!-- .navigation -->
-	<?php
-}
-endif;
-
 if ( ! function_exists( 'patch_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time and author.
@@ -901,6 +843,68 @@ if ( ! function_exists( 'patch_get_post_format_link_url' ) ) :
 		$has_url = get_url_in_content( get_the_content() );
 
 		return ( $has_url ) ? $has_url : apply_filters( 'the_permalink', get_permalink() );
+	}
+
+endif;
+
+if ( ! function_exists( 'patch_paging_nav' ) ) :
+	/**
+	 * Display navigation to next/previous set of posts when applicable.
+	 */
+	function patch_paging_nav() {
+		global $wp_query, $wp_rewrite;
+		// Don't print empty markup if there's only one page.
+		if ( $wp_query->max_num_pages < 2 ) {
+			return;
+		}
+
+		$paged        = get_query_var( 'paged' ) ? intval( get_query_var( 'paged' ) ) : 1;
+		$pagenum_link = html_entity_decode( get_pagenum_link() );
+		$query_args   = array();
+		$url_parts    = explode( '?', $pagenum_link );
+
+		if ( isset( $url_parts[1] ) ) {
+			wp_parse_str( $url_parts[1], $query_args );
+		}
+
+		$pagenum_link = remove_query_arg( array_keys( $query_args ), $pagenum_link );
+		$pagenum_link = trailingslashit( $pagenum_link ) . '%_%';
+
+		$format  = $wp_rewrite->using_index_permalinks() && ! strpos( $pagenum_link, 'index.php' ) ? 'index.php/' : '';
+		$format .= $wp_rewrite->using_permalinks() ? user_trailingslashit( $wp_rewrite->pagination_base . '/%#%', 'paged' ) : '?paged=%#%'; ?>
+
+		<nav class="pagination" role="navigation">
+			<h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'patch_txtd' ); ?></h1>
+
+			<div class="nav-links">
+
+				<?php
+				//output a disabled previous "link" if on the fist page
+				if ( $paged == 1 ) {
+					echo '<span class="prev page-numbers disabled">' . __( 'Previous', 'patch_txtd' ) . '</span>';
+				}
+
+				//output the numbered page links
+				echo paginate_links( array(
+					'base'      => $pagenum_link,
+					'format'    => $format,
+					'total'     => $wp_query->max_num_pages,
+					'current'   => $paged,
+					'prev_next' => true,
+					'prev_text' => __( 'Previous', 'patch_txtd' ),
+					'next_text' => __( 'Next', 'patch_txtd' ),
+					'add_args'  => array_map( 'urlencode', $query_args ),
+				) );
+
+				//output a disabled next "link" if on the last page
+				if ( $paged == $wp_query->max_num_pages ) {
+					echo '<span class="next page-numbers disabled">' . __( 'Next', 'patch_txtd' ) . '</span>';
+				} ?>
+
+			</div><!-- .nav-links -->
+
+		</nav><!-- .navigation -->
+	<?php
 	}
 
 endif; ?>
