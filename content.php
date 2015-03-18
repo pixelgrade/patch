8c777ec0ab0ff326f9f78f2ea93bcd8e0c8030a6
Vlad Olaru <vlad.olaru@pixelgrade.com>
2015-03-18 15:12:00 +0200

    sticky posts

--- content.php
+++ content.php
@@ -20,6 +20,11 @@
 	<?php if ( has_post_thumbnail() ) : ?>
 
 		<a href="<?php the_permalink(); ?>" <?php patch_post_thumbnail_class( 'entry-image' ); ?>>
+
+			<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+				<span class="sticky"></span>
+			<?php endif; ?>
+
 			<span class="hover" role="presentation"><?php _e( 'Read More', 'patch_txtd' ); ?></span>
 			<?php the_post_thumbnail( 'patch-masonry-image' ); ?>
 		</a>
@@ -27,6 +32,10 @@
 	<?php endif; ?>
 
 	<header <?php patch_post_title_class(); ?>>
+		
+		<?php if ( is_sticky() && ! has_post_thumbnail() && is_home() && ! is_paged() ) : ?>
+			<span class="sticky"></span>
+		<?php endif; ?>
 
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
