d9a6395a7080b91bdb343d6c615873d696a65113
Vlad Olaru <vlad.olaru@pixelgrade.com>
2015-03-19 10:33:09 +0200

    cleanup and minor bug fix

--- content-image.php
+++ content-image.php
@@ -7,33 +7,19 @@
  */
 ?>
 
-<?php if ( ! is_singular() ) { echo '<div class="grid__item">'; } ?>
+<div class="grid__item">
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<div class="entry-meta">
+		<div class="entry-meta">
 
-		<?php patch_first_category(); ?>
+			<?php patch_first_category(); ?>
 
-		<?php patch_posted_on(); ?>
+			<?php patch_posted_on(); ?>
 
-	</div><!-- .entry-meta -->
+		</div><!-- .entry-meta -->
 
-	<?php if ( has_post_thumbnail() ) : ?>
-
-		<a class="entry-image entry-image--landscape" href="<?php the_permalink(); ?>">
-
-			<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
-				<span class="sticky-post"></span>
-			<?php endif; ?>
-
-			<?php the_post_thumbnail( 'patch-masonry-image' ); ?>
-
-		</a>
-
-	<?php  else : // we need to search in the content for an image - maybe we find one
-		$first_image = patch_get_post_format_first_image();
-		if ( ! empty( $first_image ) ) : ?>
+		<?php if ( has_post_thumbnail() ) : ?>
 
 			<a class="entry-image entry-image--landscape" href="<?php the_permalink(); ?>">
 
@@ -41,25 +27,39 @@
 					<span class="sticky-post"></span>
 				<?php endif; ?>
 
-				<?php echo $first_image; ?>
+				<?php the_post_thumbnail( 'patch-masonry-image' ); ?>
 
 			</a>
 
-		<?php endif;
-	endif; ?>
+		<?php  else : // we need to search in the content for an image - maybe we find one
+			$first_image = patch_get_post_format_first_image();
+			if ( ! empty( $first_image ) ) : ?>
 
-	<header <?php patch_post_title_class(); ?>>
+				<a class="entry-image entry-image--landscape" href="<?php the_permalink(); ?>">
 
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+					<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+						<span class="sticky-post"></span>
+					<?php endif; ?>
 
-	</header><!-- .entry-header -->
+					<?php echo $first_image; ?>
 
-	<footer class="entry-footer">
+				</a>
 
-		<?php patch_entry_footer(); ?>
+			<?php endif;
+		endif; ?>
 
-	</footer><!-- .entry-footer -->
+		<header <?php patch_post_title_class(); ?>>
 
-</article><!-- #post-## -->
+			<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
-<?php if ( ! is_singular() ) { echo '</div>'; } ?>
+		</header><!-- .entry-header -->
+
+		<footer class="entry-footer">
+
+			<?php patch_entry_footer(); ?>
+
+		</footer><!-- .entry-footer -->
+
+	</article><!-- #post-## -->
+
+</div><!-- .grid__item -->
--- content.php
+++ content.php
@@ -7,60 +7,60 @@
  */
 ?>
 
-<?php if ( ! is_singular() ) { echo '<div class="grid__item">'; } ?>
+<div class="grid__item">
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<div class="entry-meta">
+		<div class="entry-meta">
 
-		<?php patch_first_category(); ?>
+			<?php patch_first_category(); ?>
 
-		<?php patch_posted_on(); ?>
+			<?php patch_posted_on(); ?>
 
-	</div><!-- .entry-meta -->
+		</div><!-- .entry-meta -->
 
-	<?php if ( has_post_thumbnail() ) : ?>
+		<?php if ( has_post_thumbnail() ) : ?>
 
-		<a href="<?php the_permalink(); ?>" <?php patch_post_thumbnail_class( 'entry-image' ); ?>>
+			<a href="<?php the_permalink(); ?>" <?php patch_post_thumbnail_class( 'entry-image' ); ?>>
 
-			<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+				<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+					<span class="sticky-post"></span>
+				<?php endif; ?>
+
+				<span class="hover" role="presentation"><?php _e( 'Read More', 'patch_txtd' ); ?></span>
+				<?php the_post_thumbnail( 'patch-masonry-image' ); ?>
+			</a>
+
+		<?php endif; ?>
+
+		<header <?php patch_post_title_class(); ?>>
+
+			<?php if ( is_sticky() && ! has_post_thumbnail() && is_home() && ! is_paged() ) : ?>
 				<span class="sticky-post"></span>
 			<?php endif; ?>
 
-			<span class="hover" role="presentation"><?php _e( 'Read More', 'patch_txtd' ); ?></span>
-			<?php the_post_thumbnail( 'patch-masonry-image' ); ?>
-		</a>
+			<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
-	<?php endif; ?>
+		</header><!-- .entry-header -->
 
-	<header <?php patch_post_title_class(); ?>>
+		<div <?php patch_post_excerpt_class(); ?>>
 
-		<?php if ( is_sticky() && ! has_post_thumbnail() && is_home() && ! is_paged() ) : ?>
-			<span class="sticky-post"></span>
-		<?php endif; ?>
+			<?php patch_post_excerpt(); ?>
 
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+			<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links"><span class="pagination-title">' . __( 'Pages:', 'patch_txtd' ),
+				'after'  => '</span></div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'patch_txtd' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) ); ?>
 
-	</header><!-- .entry-header -->
+			<?php patch_entry_footer(); ?>
 
-	<div <?php patch_post_excerpt_class(); ?>>
+		</div><!-- .entry-content -->
 
-		<?php patch_post_excerpt(); ?>
+	</article><!-- #post-## -->
 
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links"><span class="pagination-title">' . __( 'Pages:', 'patch_txtd' ),
-			'after'  => '</span></div>',
-			'link_before' => '<span>',
-			'link_after'  => '</span>',
-			'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'patch_txtd' ) . ' </span>%',
-			'separator'   => '<span class="screen-reader-text">, </span>',
-		) ); ?>
-
-		<?php patch_entry_footer(); ?>
-
-	</div><!-- .entry-content -->
-
-</article><!-- #post-## -->
-
-<?php if ( ! is_singular() ) { echo '</div>'; } ?>
+</div><!-- .grid__item -->
