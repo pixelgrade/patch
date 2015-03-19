81d6ddd2747d10ec52652dd7759ea28730f900cf
George <george.olaru@pixelgrade.com>
2015-03-19 15:39:23 +0200

    Cards Spacing #44

--- search.php
+++ search.php
@@ -13,7 +13,7 @@ get_header(); ?>
 	<?php if ( have_posts() ) : ?>
 
 		<div id="primary" class="content-area">
-			<main id="main" class="site-main grid" role="main">
+			<main id="main" class="site-main" role="main">
 
 					<?php get_template_part( 'loop' ); ?>
 
