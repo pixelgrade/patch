6653df0b7cb6cb5bcbff6069f49b486042771ed2
George <george.olaru@pixelgrade.com>
2015-03-19 13:09:21 +0200

    ++ #44

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('styles', function () {
 	return gulp.src('assets/scss/**/*.scss')
 		.pipe(sass({sourcemap: false, style: 'expanded'}))
 		.pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
-		// .pipe(cmq())
+		.pipe(cmq())
 		.pipe(csscomb())
 		.pipe(chmod(644))
 		.pipe(gulp.dest('./'));
