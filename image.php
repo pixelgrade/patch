7693cbb39c4f77164b3367a79395593a14208fee
Razvan Onofrei <razwan.me@gmail.com>
2015-05-07 17:44:20 +0300

    Revert "sixhours modification"
    This reverts commit 6c98d9ec7eb0ce5d4864b92bef4e7a33eef45abd.

--- image.php
+++ image.php
@@ -20,13 +20,13 @@ get_header(); ?>
 
 			<?php patch_the_image_navigation(); ?>
 
-			<nav class="nav-links">
-				<a class="smart-link" href="<?php echo esc_url( get_permalink( wp_get_post_parent_id( get_the_ID() ) ) ); ?>">
-
-					<?php echo sprintf( _x( 'Posted in %s', 'attachment parent post', 'patch' ), get_the_title( wp_get_post_parent_id( get_the_ID() ) ) ); ?>
-
-				</a>
-			</nav>
+			<div class="entry-content">
+				<nav class="nav-links">
+					<a href="<?php echo esc_url( get_permalink( wp_get_post_parent_id( get_the_ID() ) ) ); ?>">
+						<?php echo sprintf( _x( 'Posted in %s', 'attachment parent post', 'patch' ), get_the_title( wp_get_post_parent_id( get_the_ID() ) ) ); ?>
+					</a>
+				</nav>
+			</div>
 
 			<?php
 			// If comments are open or we have at least one comment, load up the comment template
