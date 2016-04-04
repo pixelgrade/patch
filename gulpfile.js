e4f815de3011a4c7466c8ace602b904c3a2e66ce
Vlad Olaru <vlad.olaru@pixelgrade.com>
2016-04-04 15:47:07 +0300

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -133,8 +133,7 @@ gulp.task('build', ['copy-folder'], function () {
 		files_to_remove[k] = '../build/patch/' + e;
 	});
 
-	return gulp.src(files_to_remove, {read: false})
-		.pipe(clean({force: true}));
+	return del.sync(files_to_remove, {force: true});
 });
 
 /**
