10c7ce8c49d9582a278bfd3658bb410373f54ea4
George <george.olaru@pixelgrade.com>
2017-05-22 11:43:25 +0300

    ++ #172

--- inc/integrations.php
+++ inc/integrations.php
@@ -9,8 +9,3 @@
  * Load PixCare compatibility file.
  */
 require get_template_directory() . '/inc/integrations/pixcare.php';
-
-/**
- * Load theme's configuration file (via Customify plugin)
- */
-require get_template_directory() . '/inc/integrations/pixcare.php';
