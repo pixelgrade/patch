3fcd140b0712eada8f89fdb01cd54960bb553dc1
Vlad Olaru <vlad.olaru@pixelgrade.com>
2017-09-08 12:51:11 +0300

    Update required-plugins.php

--- inc/required-plugins/required-plugins.php
+++ inc/required-plugins/required-plugins.php
@@ -26,16 +26,22 @@ function wpgrade_register_required_plugins() {
 	//			'external_url' 			=> '', // If set, overrides default API URL and points to an external URL
 	//		)
 	//	);
+	
+	$protocol = 'http:';
+	if ( is_ssl() ) {
+		$protocol = 'https:';
+	}
+	
 	$plugins = array(
 		array(
 			'name'               => 'Pixelgrade Care',
 			'slug'               => 'pixelgrade-care',
 			'force_activation'   => true,
-			'force_deactivation' => true,
+			'force_deactivation' => false,
 			'required'           => true,
-			'source'             => 'https://wupdates.com/api_wupl_version/JxbVe/2v5t1czd3vw4kmb5xqmyxj1kkwmnt9q0463lhj393r5yxtshdyg05jssgd4jglnfx7A2vdxtfdcf78r9r1sm217k4ht3r2g7pkdng5f6tgwyrk23wryA0pjxvs7gwhhb',
+			'source'             => $protocol . '//wupdates.com/api_wupl_version/JxbVe/2v5t1czd3vw4kmb5xqmyxj1kkwmnt9q0463lhj393r5yxtshdyg05jssgd4jglnfx7A2vdxtfdcf78r9r1sm217k4ht3r2g7pkdng5f6tgwyrk23wryA0pjxvs7gwhhb',
 			'external_url'       => 'https://github.com/pixelgrade/pixelgrade_care',
-			'version'            => '1.2.0',
+			'version'            => '1.2.9',
 			'is_automatic'       => true,
 		),
 		array(
