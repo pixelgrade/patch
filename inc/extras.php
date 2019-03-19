041b6cc5d4d046390c50551c04b16193e97f133f
Madalin Gorbanescu <madalin@pixelgrade.com>
2019-03-19 10:57:06 +0200

    changed quotes style

--- inc/extras.php
+++ inc/extras.php
@@ -700,7 +700,7 @@ function patch_blog_class( $class = '' ) {
  */
 
 function wrap_embed_with_div($html, $url, $attr) {
-	return "<div class=\"responsive-container\">".$html."</div>";
+	return '<div class="responsive-container">' . $html . '</div>';
 }
 
 add_filter('embed_oembed_html', 'wrap_embed_with_div', 10, 3);
